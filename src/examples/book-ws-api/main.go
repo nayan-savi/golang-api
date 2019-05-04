2992e5437956417e8ddcd6f838fdfa0a3c54fa97
Nayan Kumar H N <nayan.savi@gmail.com>
2019-05-04 05:08:26 -0700

    updated update method in api service

--- src/examples/book-ws-api/main.go
+++ src/examples/book-ws-api/main.go
@@ -54,6 +54,23 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(book)
 }
 
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Enterted to updateBook method")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+}
+
 func deleteBook(w http.ResponseWriter, r *http.Request) {
 	log.Println("Enterted to deleteBook method")
 	w.Header().Set("Content-Type", "application/json")
@@ -86,9 +103,10 @@ func main() {
 	books = append(books, Book{ID: "2", ISBN: "WERF67", Title: "First Man", Author: &Author{FirstName: "Gorge", LastName: "De"}})
 
 	sub.HandleFunc("/books", getBooks).Methods("GET")
-	sub.HandleFunc("/book/{id}", deleteBook).Methods("DELETE")
+	sub.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
 	sub.HandleFunc("/book/{id}", getBook).Methods("GET")
 	sub.HandleFunc("/book", createBook).Methods("POST")
+	sub.HandleFunc("/books/{id}", updateBook).Methods("PUT")
 	sub.HandleFunc("/index", api).Methods("GET")
 
 	// To start server
