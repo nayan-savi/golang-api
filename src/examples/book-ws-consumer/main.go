11e1e5d05f9a5d75c540f5d576f61af022d3f12a
Nayan Kumar H N <nayan.savi@gmail.com>
2019-05-04 05:59:01 -0700

    added service consume

--- src/examples/book-ws-consumer/main.go
+++ src/examples/book-ws-consumer/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func getResponse(url string) string {
@@ -22,8 +23,33 @@ func getResponse(url string) string {
 	return string(body)
 }
 
+func getResponseWithSettings(url string) string {
+	tr := &http.Transport{
+		MaxIdleConns:       10,
+		IdleConnTimeout:    1 * time.Millisecond,
+		DisableCompression: true,
+	}
+
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get(url)
+	if err != nil {
+		log.Println("Error: ", err.Error())
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Error: ", err.Error())
+		os.Exit(1)
+	}
+	return string(body)
+}
+
 func main() {
-	response := getResponse("http://localhost:8000/api/book/1")
-	log.Println(response)
+	response1 := getResponse("http://localhost:8000/api/book/1")
+	log.Println("Response ==> ",response1)
+
+	response2 := getResponseWithSettings("http://localhost:8000/api/book/2")
+	log.Println("Response ==> ",response2)
 
 }
